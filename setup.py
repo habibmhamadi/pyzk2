613522739d6b8432f49c9b0f4de746441b8f0c58
habibmhamadi <habibmhamadi@gmail.com>
2025-10-02 11:03:50 +0430

    Add source code.

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='pyzk2',
+    version='0.0.1',
+    description='Forked version of fananimi/pyzk with latest changes.',
+    url='https://github.com/habibmhamed/pyzk2',
+    author='Habib Mhamadi',
+    author_email='habibmhamadi@gmail.com',
+    license='LICENSE.txt',
+    packages=['pyzk2'],
+    keywords=[
+        'zk',
+        'pyzk',
+        'pyzk2',
+        'zksoftware',
+        'attendance machine',
+        'fingerprint',
+        'biometrics',
+        'security'
+    ],
+    zip_safe=False
+)
