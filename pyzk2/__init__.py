c3afa477837a231bf7f7341056e069bdebaf486e
habibmhamadi <habibmhamadi@gmail.com>
2025-10-02 11:41:13 +0430

    Add license file.

--- pyzk2/__init__.py
+++ pyzk2/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from .base import ZK
 
-VERSION = (0, 0, 1)
-
 __all__ = ['ZK']
 
--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    description = fh.read()
 
 setup(
     name='pyzk2',
-    version='0.0.1',
-    description='Forked version of fananimi/pyzk with latest changes.',
+    version='0.2',
     url='https://github.com/habibmhamed/pyzk2',
     author='Habib Mhamadi',
     author_email='habibmhamadi@gmail.com',
+    description='Forked version of fananimi/pyzk with latest changes.',
     license='LICENSE.txt',
-    packages=['pyzk2'],
+    packages=find_packages(),
     keywords=[
         'zk',
         'pyzk',
@@ -20,5 +23,8 @@ setup(
         'biometrics',
         'security'
     ],
-    zip_safe=False
+    zip_safe=False,
+    long_description=description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
 )
